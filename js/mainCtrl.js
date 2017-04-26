bb6f65d2964aaea21618034c8931514202b34963
Brandon Smith <Brandon Smith>
2017-04-26 15:17:32 -0600

    Solution

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,31 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular.module('chatroom').controller('mainCtrl', function ($scope, messageService) {
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    //The getMessages function will call the getMessages method on the messageService object.
+    //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
+    $scope.getMessages = function () {
+        messageService.getMessages().then(function (response) {
+            console.log(response.data);
+            $scope.messages = response.data;
+        })
+    }
+
+    $scope.getMessages();
 
 
 
+    //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
+    //pass that text to the postMessage method on the messageService object which will
+    //then post it to the backend.
+    $scope.postMessage = function (post) {
+        messageService.postMessage(post).then(function (response) {})
+    }
 
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
 
+    //uncomment this code when your getMessages function is finished
+    //This goes and gets new data every second, which mimicking a chat room experience.
+    setInterval(function () {
+        $scope.getMessages();
 
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+    }, 500)
 
 })
